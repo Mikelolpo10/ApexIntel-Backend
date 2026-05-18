8a5f44ed4bf5a46ac7208ee966b357d0ec3086f2
Mikelolpo10 <xzvmike@gmail.com>
2026-05-18 20:29:05 +0700

    Feature: added and implemented cors library

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+import cors from 'cors'
 import express from 'express'
 import legendsRouter from './routers/legends.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.use(cors())
 
 app.use('/legends', legendsRouter)
 
