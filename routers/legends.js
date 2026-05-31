25559f3b94538842adf44648c923ea528f9c1882
Mikelolpo10 <xzvmike@gmail.com>
2026-05-31 10:09:12 +0700

    Feature: only send full data if query == fulldata

--- routers/legends.js
+++ routers/legends.js
@@ -5,7 +5,23 @@ import legendsClass from '../data/legendsClass.json' with { type: 'json' }
 const router = express.Router()
 
 router.get('/legendsdata', (req, res) => {
-  res.json(legendsData)
+  const expand = req.query.expand
+
+  if (expand === 'fulldata') {
+    res.status(200)
+    return res.json(legendsData)
+  }
+
+  const result = legendsData.map(legend => {
+    const data = {
+      name: legend.name,
+      path: legend.path,
+      img: legend.img,
+    }
+
+    return data
+  })
+  res.json(result)
 })
 
 router.get('/legendsdata/:name', (req, res) => {
