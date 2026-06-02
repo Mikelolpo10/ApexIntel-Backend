d6ac9ef09609d24583f802b4bd3989abd01b2ff0
Mikelolpo10 <xzvmike@gmail.com>
2026-06-02 11:48:54 +0700

    Fix: file import error

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import cors from 'cors'
 import express from 'express'
-import legendsRouter from './routers/legends.js'
+import legendsRouter from './routers/legends.ts'
 
 const app = express()
 const PORT = process.env.PORT || 3000
