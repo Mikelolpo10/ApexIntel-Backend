20bdf0edca48b82f4ddd42edff58d1edb2b6bb32
Mikelolpo10 <xzvmike@gmail.com>
2026-06-02 08:32:12 +0700

    Refactor: all old code to typescript types and interface

--- routers/legends.ts
+++ routers/legends.ts
@@ -4,16 +4,45 @@ import legendsClass from '../data/legendsClass.json' with { type: 'json' }
 
 const router = express.Router()
 
-router.get('/legendsdata', (req, res) => {
-  const expand = req.query.expand
+type PartialData = {
+  name: string;
+  path: string;
+  img: string;
+}
+
+interface Ability {
+  img: string;
+  name: string;
+  description: string;
+}
+
+interface Legend {
+  name: string;
+  realName: string;
+  nickname: string;
+  quote: string;
+  path: string;
+  img: string;
+  class: string;
+  gender: string;
+  age: string;
+  homeworld: string;
+  passive: Ability;
+  tactical: Ability;
+  ultimate: Ability;
+  description: string;
+}
+
+router.get('/legendsdata', (req, res): void => {
+  const expand = req.query.expand as string | undefined
 
   if (expand === 'fulldata') {
     res.status(200)
-    return res.json(legendsData)
+    res.json(legendsData as Legend[])
   }
 
   const result = legendsData.map(legend => {
-    const data = {
+    const data: PartialData = {
       name: legend.name,
       path: legend.path,
       img: legend.img,
@@ -24,8 +53,8 @@ router.get('/legendsdata', (req, res) => {
   res.json(result)
 })
 
-router.get('/legendsdata/:name', (req, res) => {
-  const legendData = legendsData.find(legend => legend.name.toLowerCase() === req.params.name)
+router.get('/legendsdata/:name', (req, res): void => {
+  const legendData: Legend | undefined = legendsData.find(legend => legend.name.toLowerCase() === req.params.name)
 
   res.json(legendData)
 })
